ec4fb5d4120ca5a68235573f3d2f280d5e851daf
Thanvi <thanvi.ch2007@gmail.com>
2025-12-13 16:34:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -12,14 +12,15 @@ df['TotalSpending'] = df[spending_cols].sum(axis=1)
 
 #Handling misiing values in column cabin
 
-df[['Deck','Cabin_Num','Side']]=df['Cabin'].str.split('/',expand=True)
-df['Group']=df['PassengerId'].str.split('_').str[0]
-df['Cabin_Num'] = pd.to_numeric(df['Cabin_Num'], errors='coerce')
-df['Deck'] = df.groupby('Group')['Deck'].transform(lambda x: x.fillna(x.mode()[0] if len(x.mode()) else x))
+df[['Deck','CabinNum','Side']]=df['Cabin'].str.split('/',expand=True)
+df['GroupId']=df['PassengerId'].str.split('_').str[0]
+df['CabinNum'] = pd.to_numeric(df['CabinNum'], errors='coerce')
+df['Deck'] = df.groupby('GroupId')['Deck'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x))
 deck_mode = df['Deck'].mode()[0]
 df['Deck'] = df['Deck'].fillna(deck_mode)
-cabin_median = df['Cabin_Num'].median()
-df['Cabin_Num'] = df['Cabin_Num'].fillna(cabin_median)
-df['Side'] = df.groupby('Group')['Side'].transform(lambda x: x.fillna(x.mode()[0] if len(x.mode()) else x))
+cabin_median = df['CabinNum'].median()
+df['CabinNum'] = df['CabinNum'].fillna(cabin_median)
+df['Side'] = df.groupby('GroupId')['Side'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x))
 side_mode = df['Side'].mode()[0]
 df['Side'] = df['Side'].fillna(side_mode)
+df['GroupSize'] = df.groupby('GroupId')['PassengerId'].transform('count')

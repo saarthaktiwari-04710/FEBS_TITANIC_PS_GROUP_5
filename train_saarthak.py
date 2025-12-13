0f2d77b31e3ae7522339442bffc244c9f5b35291
saarthaktiwari-04710 <saarthaktiwari7@gmail.com>
2025-12-13 16:03:51 +0530

    Handled missing values

--- train_saarthak.py
+++ train_saarthak.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 def preprocess_saarthak(df):
-si_mostfrequent=SimpleImputer(strategy='most_frequent')
+    si_mostfrequent=SimpleImputer(strategy='most_frequent')
+    si_missing=SimpleImputer(strategy='constant',fill_value='Missing'
     df[['FoodCourt']] = si_mostfrequent.fit_transform(df[['FoodCourt']])
     df[['RoomService']] = si_mostfrequent.fit_transform(df[['RoomService']])
     df[['CryoSleep']] = si_mostfrequent.fit_transform(df[['CryoSleep']])
-    df['Name'] = df['Name'].fillna("Missing")
+    df[['Name']]=si_missing.fit_transform(df[['Name']])
 return df

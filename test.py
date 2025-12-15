1defbd193cddb0bf679d0c7854f139e64ff69e1f
saarthaktiwari-04710 <saarthaktiwari7@gmail.com>
2025-12-15 20:32:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,30 @@
 import pandas as pd
 import numpy as np
 import pickle
-import preprocessing_saarthak
-from preprocessing_TC import preprocess_TC
 import train
+import preprocessing_saarthak
+from preprocessing_TC import preprocess
 df = pd.read_csv("test.csv")
 
-df = preprocess_TC(df)
+df = preprocess(df)
 
 
-df[['RoomService']]=si_RS.transform(df[['RoomService']])
-df[['FoodCourt']]=si_FC.transform(df[['FoodCourt']])
-df[['Spa']]=si_SPA.transform(df[['Spa']])
-df[['CryoSleep']]=si_CS.transform(df[['CryoSleep']])
-df[['Name']]=si_Name.transform(df[['Name']])
+df[['RoomService']]=preprocessing_saarthak.si_RS.transform(df[['RoomService']])
+df[['FoodCourt']]=preprocessing_saarthak.si_FC.transform(df[['FoodCourt']])
+df[['Spa']]=preprocessing_saarthak.si_SPA.transform(df[['Spa']])
+df[['CryoSleep']]=preprocessing_saarthak.si_CS.transform(df[['CryoSleep']])
+df[['Name']]=preprocessing_saarthak.si_Name.transform(df[['Name']])
+
 
 df[['Deck','CabinNum','Side']]=df['Cabin'].str.split('/',expand=True)
 df['GroupId']=df['PassengerId'].str.split('_').str[0]
 df['CabinNum'] = pd.to_numeric(df['CabinNum'], errors='coerce')
-df['Deck'] = df.groupby('GroupId')['Deck'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x))
+df['Deck'] = df.groupby('GroupId')['Deck'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x)
 deck_mode = df['Deck'].mode()[0]
 df['Deck'] = df['Deck'].fillna(deck_mode)
 cabin_median = df['CabinNum'].median()
 df['CabinNum'] = df['CabinNum'].fillna(cabin_median)
-df['Side'] = df.groupby('GroupId')['Side'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x))
+df['Side'] = df.groupby('GroupId')['Side'].transform(lambda x: x.fillna(x.mode()[0]) if not x.mode().empty else x)
 side_mode = df['Side'].mode()[0]
 df['Side'] = df['Side'].fillna(side_mode)
 df['GroupSize'] = df.groupby('GroupId')['PassengerId'].transform('count')
@@ -31,6 +32,7 @@ df['GroupSize'] = df.groupby('GroupId')['PassengerId'].transform('count')
 
 spending_cols = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 df['TotalSpending'] = df[spending_cols].sum(axis=1)
+passenger_ids = df['PassengerId']
 
 # Dropping columns that are not useful 
 df = df.drop(columns=['PassengerId','Name', 'Cabin'])

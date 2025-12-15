2d2ac73408991e815f7e55110ca695c39c1c14b3
saarthaktiwari-04710 <saarthaktiwari7@gmail.com>
2025-12-15 14:46:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 import pickle
+import preprocessing_saarthak
 
 df = pd.read_csv("test.csv")
+df['RoomService']=si_RS.transform(df['RoomService'])
+df['FoodCourt']=si_FC.transform(df['FoodCourt'])
+df['Spa']=si_S.transform(df['Spa'])
+df['CryoSleep']=si_CS.transform(df['CryoSleep'])
+df['Name']=si_Name.transform(df['Name'])
 
 spending_cols = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 df['TotalSpending'] = df[spending_cols].sum(axis=1)
